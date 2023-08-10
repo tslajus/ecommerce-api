0a4405b11c28d6fa8f409bbc0d3014f120232e56
Tadas Šlajus <tslajus@gmail.com~>
2023-08-10 15:10:47 +0300

    Initial commit;

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,23 +1,26 @@
-// import express from "express";
-// import { Router } from "express";
-// import { authenticateUser, authorizePermissions } from "@/middleware/authentication";
+import express from "express";
+import { Router } from "express";
+import {
+  authenticateUser,
+  authorizePermissions,
+} from "@/middleware/authentication";
 
-// import {
-//   getAllUsers,
-//   getSingleUser,
-//   showCurrentUser,
-//   updateUser,
-//   updateUserPassword,
-// } from "@/controllers/User";
+import {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+} from "@/controllers/User";
 
-// const router: Router = express.Router();
+const router: Router = express.Router();
 
-// router
-//   .route("/")
-//   .get(authenticateUser, authorizePermissions("admin"), getAllUsers);
-// router.route("/:id").get(authenticateUser, getSingleUser);
-// router.route("/profile").get(authenticateUser, showCurrentUser);
-// router.route("/updateUserProfile").post(authenticateUser, updateUser);
-// router.route("/updateUserPassword").post(authenticateUser, updateUserPassword);
+router
+  .route("/")
+  .get(authenticateUser, authorizePermissions("admin"), getAllUsers);
+router.route("/:id").get(authenticateUser, getSingleUser);
+router.route("/profile").get(authenticateUser, showCurrentUser);
+router.route("/updateUserProfile").post(authenticateUser, updateUser);
+router.route("/updateUserPassword").post(authenticateUser, updateUserPassword);
 
-// export default router;
+export default router;
